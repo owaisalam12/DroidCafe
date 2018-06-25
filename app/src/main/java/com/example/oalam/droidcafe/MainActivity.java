192968f9296db688b47a2c4cce6147f0dba0d1ee
Owais Alam <owaisalamkhi@gmail.com>
2018-06-25 22:26:48 +0500

    second commit

---
+++ app/src/main/java/com/example/oalam/droidcafe/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.oalam.droidcafe;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message,
+                Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     * Shows a message that the donut image was clicked.
+     */
+    public void showDonutOrder(View view) {
+        showFoodOrder(getString(R.string.donut_order_message));
+    }
+
+    /**
+     * Shows a message that the ice cream sandwich image was clicked.
+     */
+    public void showIceCreamOrder(View view) {
+        showFoodOrder(getString(R.string.ice_cream_order_message));
+    }
+
+    /**
+     * Shows a message that the froyo image was clicked.
+     */
+    public void showFroyoOrder(View view) {
+        showFoodOrder(getString(R.string.froyo_order_message));
+    }
+
+    /**
+     * Displays a toast message for the food order
+     * and starts the OrderActivity activity.
+     *
+     * @param message Message to display.
+     */
+    public void showFoodOrder(String message) {
+        displayToast(message);
+        Intent intent = new Intent(this, OrderActivity.class);
+        startActivity(intent);
+    }
+}
